ec5f6231cfdd113728840771100fe2b3753f6365
shruti2522 <shruti.apc01@gmail.com>
2024-04-01 05:56:51 +0530

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-#![no_std]
-#![no_main]
-#![feature(custom_test_frameworks)]
+#![no_std] 
+
+#![no_main] 
+
+#![feature(custom_test_frameworks)] 
+
+// specifies the test runner function for the crate.
 #![test_runner(ros::test_runner)]
+
+// specifies the entry point for the test harness.
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
-use ros::println;
+use core::panic::PanicInfo; 
+use ros::println;.
 
+// panic handler for non-test mode.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -14,20 +21,25 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+// panic handler for test mode.
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     ros::test_panic_handler(info)
 }
 
+// entry point for the program.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    
     #[cfg(test)]
     test_main();
+    
     loop{}
 }
 
+// test case demonstrating a trivial assertion.
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
