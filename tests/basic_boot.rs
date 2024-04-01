47b0410d6b6f3170bdace290e97d288b989ba31b
shruti2522 <shruti.apc01@gmail.com>
2024-04-01 06:27:36 +0530

    updated basic_boot.rs

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,24 +1,29 @@
-#![no_std]
-#![no_main]
-#![feature(custom_test_frameworks)]
+#![no_std] 
+#![no_main] 
+#![feature(custom_test_frameworks)] 
+
 #![test_runner(ros::test_runner)]
+
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
-use ros::println;
+use core::panic::PanicInfo; 
+use ros::println; 
 
+// entry point for the program.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  test_main();
-  loop{}
+    test_main();
+    loop {}
 }
 
+// panic handler for testing.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-  ros::test_panic_handler(info)
+    ros::test_panic_handler(info)
 }
 
+// test case for println macro.
 #[test_case]
 fn test_println() {
-  println!("test_println output");
+    println!("test_println output");
 }
